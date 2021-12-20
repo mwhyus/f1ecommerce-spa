430e87804c0e0073e3367eff88a8fea37910e050
mwhyus <mwahyus17@gmail.com>
2021-12-20 09:30:19 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import f1 from './image/f1.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="Parentbox">
+      <div class='Photo'>
+        <img src={f1} />
+      </div>
+      <div class='Descrption'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
